aa73dfcc10fcd1728e07a323b6967917f6f77f97
Steve Wilson <swilson@mojoware.com>
2019-05-15 17:52:08 -0500

    Remove dead code.

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -4,12 +4,4 @@ class AppController < ApplicationController
     redirect_to Url.find_by_short_url(params[:id]).long_url
   end
 
-  def original_link
-
-  end
-
-  def get_link
-    @short_link = Url.new
-  end
-
 end
